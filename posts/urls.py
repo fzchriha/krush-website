fb3d0327010ccd072135d611b8b0b07c0e9ae607
Fatima Zahra Chriha <fatimazahra.chriha@gmail.com>
2020-07-10 20:08:22 -0400

    Add friends and notifications

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from .views import home_view, CreateCrush, detail_view, tagged
-from django.conf.urls import url
 
-url(r'^connect/(?P<operation>.+)/(?P<pk>\d+)/$', views.change_friends, name='change_friends')
+
+#posts
+path('crush/', home_view, name='crush'),
+path('crush/create/', CreateCrush, name="create-crush"),
+path('post/<slug:slug>/', detail_view, name="detail"),
+path('tag/<slug:slug>/', tagged, name="tagged"),
+
