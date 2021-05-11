885b1f067f3cef5a2b66b2d43ae7fdc321896792
Fatima Zahra Chriha <30349896+fzchriha@users.noreply.github.com>
2021-05-11 12:09:06 -0400

    Update forms.py

--- posts/forms.py
+++ posts/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Post
 
+# This form is for creating the profile of the crush (name, nickname, content, traits/tags)
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
